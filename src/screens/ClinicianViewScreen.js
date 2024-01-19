bd05353a326d7a3549c1f397c60a7c8dd672efe1
Mark Peschel <mpeschel10@gmail.com>
2024-01-19 17:44:28 -0500

    Add thumbnail slot to patient page. Add face-f-at-rest placeholder icon.

--- src/screens/ClinicianViewScreen.js
+++ src/screens/ClinicianViewScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
 
 import state from '../state';
 import UnreadBadge from '../components/UnreadBadge';
@@ -7,31 +7,52 @@ import UnreadBadge from '../components/UnreadBadge';
 const ClinicianViewScreen = ({ navigation }) => {
 
   const patientItems = state.demoPatientMessages.map(({name, unread}) => {
-    return (<View key={name} style={styles.patientRow}>
-      <View>
-        <Button title={name} onPress={() => navigation.navigate('ClinicianViewInfo')} />
-        <UnreadBadge value={unread} />
-      </View>
-    </View>)
-
+    return (
+      <Pressable
+        key={name}
+        style={styles.patientContainer} onPress={() => navigation.navigate('ClinicianViewInfo')}
+      >
+        <Text style={styles.patientName}>{name}</Text>
+        <View>
+          <Image
+            style={styles.patientThumbnail}
+            source={require('/assets/face-f-at-rest.png')}
+          />
+          <UnreadBadge value={unread} />
+        </View>
+      </Pressable>
+    );
   })
 
   return (
     <View>
-      <Text style={{
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-      }}>Patients</Text>
+      <Text style={styles.h1}>Patients</Text>
       <View>{patientItems}</View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  patientRow: {
+  h1: {
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 10,
+  }, patientContainer: {
       flexDirection: 'row',
       alignItems: 'center',
+      marginVertical: 10,
+      marginHorizontal: 40,
+      padding: 5,
+      borderColor: '#2060dd',
+      borderWidth: 2,
+      borderRadius: 5,
+  }, patientThumbnail: {
+    width: 90,
+    height: 90,
+  }, patientName: {
+    flexGrow: 1,
+    textAlign: 'center',
+    fontSize: 35,
   }
 });
 
