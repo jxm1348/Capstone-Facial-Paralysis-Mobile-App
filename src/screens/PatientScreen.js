3c78f096276d3f18c3558f22708bf08c35db974c
Mark Peschel <mpeschel10@gmail.com>
2024-01-20 14:20:14 -0500

    Visiting Patient now clears unread for that patient. Unfortunately, this introduces a bug: stack navigator caches the page if it's in the stack, so navigating up and down shows stale unread numbers. Moved demoGetUnreadPatient into state declaration so I get type hints.

--- src/screens/PatientScreen.js
+++ src/screens/PatientScreen.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Text, View, Button } from 'react-native';
 
 import globalStyles from '../globalStyles';
+import state from '../state';
 
 const PatientScreen = ({navigation, route}) => {
   const { name } = route.params;
+  const patient = state.demoGetPatientByName(name);
+  for (const message of patient.messages) {
+    message.read = true;
+  }
   
   return (
     <View>
--- src/state.js
+++ src/state.js
@@ -17,17 +17,16 @@ const state = {
         {name:'Owen Wilson', messages: [
         ]},
       ],
-};
-
-export function init() {
-    state.demoGetUnreadPatient = patient => {
+    demoGetUnreadPatient(patient) {
         let total = 0;
         for (const {read} of patient.messages) {
             total += read ? 0 : 1;
         }
-        console.log("Got unread count for patient", patient.name, "value", total);
         return total;
     }
+};
+
+export function init() {
 
     state.demoGetUnreadTotal = () => state.demoPatients.reduce(
         (acc, patient) => acc + state.demoGetUnreadPatient(patient), 0
