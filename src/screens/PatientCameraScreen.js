c58bf94045f954c3292ff13af9df2e895b30c16d
Mark Peschel <mpeschel10@gmail.com>
2024-01-22 17:57:02 -0500

    Split camera from permission requesting component, add takePicture button

--- src/screens/PatientCameraScreen.js
+++ src/screens/PatientCameraScreen.js
@@ -1,35 +1,51 @@
-import {Image, StyleSheet, Text, View } from 'react-native';
+import {Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 //https://www.freecodecamp.org/news/how-to-create-a-camera-app-with-expo-and-react-native/
 
-const PatientUploadScreen = ({ navigation }) => {
+function CameraYes() {
+  const cameraRef = useRef(null);
+
+  function takePicture() {
+    // Do something here
+    
+    console.log(cameraRef.current);
+  }
+
+  return (<Camera
+    type={CameraType.front}
+    style={styles.camera}
+    ref={cameraRef}
+  >
+    <Pressable style={{
+      width: 100, height:100, borderRadius: 50, backgroundColor: 'magenta',
+      marginBottom: 30
+    }} onPress={() => takePicture()}></Pressable>
+  </Camera>);
+}
+
+function CameraMaybe() {
   const [permissionsRequest, setPermissionsRequest] = useState(null);
   useEffect(() => {
       Camera.requestCameraPermissionsAsync()
       .then(permissionsRequest => setPermissionsRequest(permissionsRequest));
   }, []);
 
-  let camera;
   if (permissionsRequest === null) {
-    camera = <Text>Awaiting camera permissions.</Text>;
+    return <Text>Awaiting camera permissions.</Text>;
   } else if (permissionsRequest.status === "granted") {
-    camera = (<Camera
-      type={CameraType.front}
-      style={styles.camera}
-      ref={(r) => {
-        const cam = r // Comment this line out
-      }}
-    ></Camera>);
+    return <CameraYes />;
   } else if (permissionsRequest.status === "denied") {
-    camera = <Text>This app cannot take your picture without camera permissions.</Text>;
+    return <Text>This app cannot take your picture without camera permissions.</Text>;
   } else {
-    camera = <Text>Camera permissions request status is unexpected value {permissionsRequest.status}</Text>;
+    return <Text>Camera permissions request status is unexpected value {permissionsRequest.status}</Text>;
   }
+}
 
+const PatientUploadScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      {camera}
+      <CameraMaybe />
     </View>
   );  
 };
@@ -45,6 +61,8 @@ const styles = StyleSheet.create({
     flex: 1,
     width:"100%",
     height:"90%",
+    alignItems: 'center',
+    justifyContent: 'end',
   }
 });
 
