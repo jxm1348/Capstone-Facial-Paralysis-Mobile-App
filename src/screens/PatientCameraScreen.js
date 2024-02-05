186236fab505279a0283be6da85fb9235d47e9f2
Mark Peschel <mpeschel10@gmail.com>
2024-02-05 17:27:40 -0500

    Add stackoverflow copypasta for extracting the blob from the datauri.

--- src/screens/PatientCameraScreen.js
+++ src/screens/PatientCameraScreen.js
@@ -8,11 +8,45 @@ function CameraYes({imageKey}) {
   const navigation = useNavigation();
   const cameraRef = useRef(null);
 
+  // Credit to "Matěj Pokorný" of https://stackoverflow.com/users/2438165/mat%c4%9bj-pokorn%c3%bd
+  // Via https://stackoverflow.com/a/18197341/6286797
+  function download(filename, blob) {
+    const element = document.createElement('a');
+    element.setAttribute('href', window.URL.createObjectURL(blob));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+
+  // Credit to "devnull69" of https://stackoverflow.com/users/1030974/devnull69
+  // Via https://stackoverflow.com/a/12300351/6286797
+  function dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',', 1)[0].split(':')[1].split(';')[0];
+  
+    // write the bytes of the string to an ArrayBuffer
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+  
+    return new Blob([ab], {type: mimeString});
+  }
+
   async function takePicture() {
     if(cameraRef.current){
       let photo = await cameraRef.current.takePictureAsync();
-      state.workingMessage.images[imageKey] = photo.uri;
-      navigation.navigate("PatientUpload");
+      download("image_i_hope.png", dataURItoBlob(photo.uri));
+      return;
+      // state.workingMessage.images[imageKey] = photo.uri;
+      // navigation.navigate("PatientUpload");
     }
   }
 
