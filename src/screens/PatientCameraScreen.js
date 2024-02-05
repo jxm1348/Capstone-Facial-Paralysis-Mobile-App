cc182b71adf5ef2622f50a86fee888c019b0675a
Mark Peschel <mpeschel10@gmail.com>
2024-02-04 19:41:13 -0500

    Demonstrate setting image uri from camera.

--- src/screens/PatientCameraScreen.js
+++ src/screens/PatientCameraScreen.js
@@ -6,13 +6,16 @@ import state from '../state';
 function CameraYes() {
   const cameraRef = useRef(null);
 
+  const [ imageUri, setImageUri ] = useState(undefined);
+  console.log("Image uri is ", imageUri);
 
   async function takePicture() {
     // console.log(cameraRef.current);
     if(cameraRef.current){
       let photo = await cameraRef.current.takePictureAsync();
-      // console.log(photo.uri);
+      console.log(photo.uri);
       state.patient.workingPhotoSet[0] = photo.uri;  
+      setImageUri(photo.uri);
       //TODO receive state data from PatientUploadScreen to determine index of overwrite, and have PatientUploadScreen take image from state if not null
       //TODO overlay outline on camera using same index, maybe make versions of the face images that are just outlines? need to see it in action firstgit
     }
@@ -23,6 +26,13 @@ function CameraYes() {
     style={styles.camera}
     ref={cameraRef}
   >
+    <View style={{
+      position: 'absolute',
+      top: 40,
+      left: 120,
+      backgroundColor: 'red',
+      padding: 30,
+    }}><Image source={{uri: imageUri}} style={{width:200, height:200}}></Image></View>
     <View style={{
       width: "55vh", height:"70vh", borderRadius: "50%", border: "5px solid white",
       marginBottom: 30
