86b80d5728d56b57bd1f48bba32452a1ccc220df
Mark Peschel <mpeschel10@gmail.com>
2024-02-05 12:59:24 -0500

    Add test demonstrating upload, get url

--- src/state.test.mjs
+++ src/state.test.mjs
@@ -6,7 +6,8 @@ import {
     terminate,
 } from 'firebase/firestore';
 
-import state from './state.mjs';
+import state, { db, storage } from './state.mjs';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 describe('Firebase', () => {
     const testDoc = {name: 'fewljwkfeewflkjfds'};
@@ -15,31 +16,48 @@ describe('Firebase', () => {
     });
 
     it('Can save a document to the test collection', async () => {
-        await addDoc(collection(state.db, 'test'), testDoc);
+        await addDoc(collection(db, 'test'), testDoc);
         expect(
-            (await getCountFromServer(collection(state.db, 'test'))).data().count
+            (await getCountFromServer(collection(db, 'test'))).data().count
         ).toBeGreaterThan(0);
     });
 
     it('Can read a saved document', async () => {
-        const querySnapshot = await getDocs(collection(state.db, 'test'));
+        const querySnapshot = await getDocs(collection(db, 'test'));
         querySnapshot.forEach(document => expect(document.data()).toStrictEqual(testDoc));
         // You can also use document.id for stuff
     });
 
     it('Can delete all docs in the test collection', async () => {
-        const querySnapshot = await getDocs(collection(state.db, 'test'));
+        const querySnapshot = await getDocs(collection(db, 'test'));
         const promises = querySnapshot.docs.map(document => deleteDoc(
-            doc(state.db, 'test', document.id)
+            doc(db, 'test', document.id)
         ));
         await Promise.all(promises);
 
         expect(
-            (await getCountFromServer(collection(state.db, 'test')))
+            (await getCountFromServer(collection(db, 'test')))
                 .data().count
         ).toStrictEqual(0);
     });
 });
+
+describe('Google Cloud Storage', () => {
+    it('Can upload a file', async () => {
+        const file = new Blob(['Specious correlations'], {type:'text/plain'});
+        const uploadRef = ref(storage, "test_files/test.txt");
+        const snapshot = await uploadBytes(uploadRef, file, {
+            contentType: 'text/plain',
+        });
+        console.log("Upload success! Snapshot: ", snapshot);
+        
+        const newRef = ref(storage, 'test_files/test.txt');
+        const downloadUrl = await getDownloadURL(newRef);
+        console.log("Download url = ", downloadUrl);
+        // Note: You cannot download using fetch or getBlob due to CORS.
+        // Image display should still work, though.
+    });
+})
 
 afterAll(() => {
     terminate(state.db);
