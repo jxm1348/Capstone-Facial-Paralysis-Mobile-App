fd6e039b7ff0703922bf1d0dd2a0af9c55036f9d
Mark Peschel <mpeschel10@gmail.com>
2024-02-04 19:29:42 -0500

    Make a bunch of random changes to PatientUploadScreen in advance of requirements. Add useState image list in the hopes that we can somehow pass it along to the camera screen, which I now realize is probably impossible. Make keyed list of thumbnails, which I think may actually be necessary going forwards.

--- src/screens/PatientUploadScreen.js
+++ src/screens/PatientUploadScreen.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { View, ScrollView, TouchableHighlight, Text, StyleSheet, Image, Button } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, Pressable, Text, StyleSheet, Image, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
 import ActionButton from '../components/ActionButton';
 import NavigationBar from '../components/NavigationBar';
+import globalStyles from '../globalStyles';
 
 const PatientUploadScreen = () => {
   const navigation = useNavigation();
 
-  const items = Array.from({ length: 8 }, (_, index) => index + 1);
+  // Enforces an order on the images.
+  const [ imageKeys, setImageKeys ] = useState(() => (
+    ['at-rest', 'eyebrows-up', 'eyes-closed', 'nose-wrinkle', 'big-smile', 'lips-puckered', 'lower-teeth-bared']
+  ));
+  // setImageKeys will be called when I add the button for adding more images to the report at the end.
 
+  const [ images, setImages ] = useState(() => ({
+    'at-rest': require('../resources/face-f-at-rest.png'),
+    'eyebrows-up': require('../resources/face-f-eyebrows-up.png'),
+    'eyes-closed': require('../resources/face-f-eyes-closed.png'),
+    'nose-wrinkle': require('../resources/face-f-nose-wrinkle.png'),
+    'big-smile': require('../resources/face-f-big-smile.png'),
+    'lips-puckered': require('../resources/face-f-lips-puckered.png'),
+    'lower-teeth-bared': require('../resources/face-f-lower-teeth-bared.png'),
+  }));
 
-  const images = [
-    require('../resources/face-f-at-rest.png'),
-    require('../resources/face-f-big-smile.png'),
-    require('../resources/face-f-eyebrows-up.png'),
-    require('../resources/face-f-eyes-closed.png'),
-    require('../resources/face-f-lips-puckered.png'),
-    require('../resources/face-f-lower-teeth-bared.png'),
-    require('../resources/face-f-nose-wrinkle.png'),
-    require('../resources/face-f-root.png')
-  ];
+  const currySetImage = (key) => {
+    return (value) => {
+      const change = {}; change[key] = value;
+      setImages(Object.assign({}, images, change));
+    }
+  }
+
+  const thumbnails = imageKeys.map(key => (
+    <Pressable
+      key={key}
+      style={styles.touchableItem}
+      underlayColor="#ddd"
+      onPress={() => navigation.navigate('PatientCamera', key)}
+    >
+      <>
+        <Image source={images[key]} style={styles.itemImage} />
+        <Text style={styles.itemText}>{key}</Text>
+      </>
+    </Pressable>
+  ));
 
   return (
     <View style={{ flex: 1 }}>
@@ -30,26 +55,8 @@ const PatientUploadScreen = () => {
         showsVerticalScrollIndicator={false}
         decelerationRate="fast"
       >
-        {items.map((item, index) => (
-          <TouchableHighlight
-            key={item}
-            style={styles.touchableItem}
-            underlayColor="#ddd"
-            onPress={() => navigation.navigate('PatientCamera', {item})}
-          >
-            <>
-              <Image source={images[index % images.length]} style={styles.itemImage} />
-              <Text style={styles.itemText}>{`Item ${item}`}</Text>
-            </>
-          </TouchableHighlight>
-        ))}
-        <Button 
-        title="Upload"
-        style={
-          {
-            marginTop: 3,
-          }
-        }>Upload</Button>
+        {thumbnails}
+        <Pressable style={globalStyles.button}><Text>Upload</Text></Pressable>
       </ScrollView>
       <ActionButton title="Go Back" onPress={() => navigation.goBack()} />
     </View>
