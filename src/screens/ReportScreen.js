ace856b28964549082dba8e78aadf1df02705b50
Mark Peschel <mpeschel10@gmail.com>
2024-02-28 19:23:57 -0500

    ReportScreen loads dynamic ref /images/uid instead of static uri from message.

--- src/screens/ReportScreen.js
+++ src/screens/ReportScreen.js
@@ -1,37 +1,64 @@
 import { useState, useEffect } from 'react';
 import { Text, View, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { getDoc, doc, updateDoc } from 'firebase/firestore';
+import { ref, getDownloadURL } from 'firebase/storage';
 
 import globalStyles from '../globalStyles';
-import state from '../state.js';
+import { storage, db } from '../state.js';
 import { imageKeyOrder } from '../constants';
 
 function ReportSkeleton() {
     return <Text>Loading...</Text>;
 }
 
+function Tile3({style, uid, imageName}) {
+    const [ uri, setUri ] = useState(undefined);
+    useEffect(() => {(async () => {
+        const imageRef = ref(storage, `images/${uid}/${imageName}`);
+        setUri(await getDownloadURL(imageRef));
+    })();}, []);
+    
+    if (uri === undefined) {
+        return <Text>Loading image...</Text>;
+    } else {
+        return <Image style={style} source={{uri}} key={imageName} />;
+    }
+}
+
+function getImageComponents(message) {
+    const {width, height} = Dimensions.get('window');
+    if (message.messageVersion === undefined) {
+        const sourceIterator = message.images.map ? message.images :
+            imageKeyOrder.map(key => message.images[key]).filter(value => value);
+        
+        return sourceIterator.map(source => 
+            <Image style={{width, height: width}} source={{uri: source}} key={source}/>
+        );
+    } else if (message.messageVersion === 3) {
+        return imageKeyOrder
+            .map(key => message.images[key])
+            .filter(value => value)
+            .map(imageName => {
+                return <Tile3 style={{width: width, height: width / 3 * 4}} uid={message.from} imageName={imageName} key={imageName}/>;
+            });
+    } else {
+        return [ <Text key={1}>Unknown message version</Text> ];
+    }
+}
+
 function Report(message) {
     if (!message) return <ReportSkeleton />;
 
     if (!message.read) {
-        updateDoc(doc(state.db, 'messages', message.id), {read:true}).then(result => {
+        updateDoc(doc(db, 'messages', message.id), {read:true}).then(result => {
             console.log("Set read on message", message.id);
         });
     }
     
-    const {width, height} = Dimensions.get('window');
-
-    const sourceIterator = message.images.map ? message.images :
-        imageKeyOrder.map(key => message.images[key]).filter(value => value);
-    
-    const imageComponents = sourceIterator.map(source => 
-        <Image style={{width, height: width}} source={{uri: source}} key={source}/>
-    );
-
     return (<>
         <Text style={{textAlign: 'center'}}>{message.date}</Text>
         <View style={styles.images}>
-            {imageComponents}
+            {getImageComponents(message)}
         </View>
         {message.message !== null && 
             <Text style={globalStyles.h2}>{message.message}</Text>
@@ -46,7 +73,7 @@ export default function ReportScreen({navigation, route}) {
     const [ message, setMessage ] = useState(undefined);
     
     useEffect(() => {
-        getDoc(doc(state.db, 'messages', id))
+        getDoc(doc(db, 'messages', id))
             .then(snapshot => {
                 const result = snapshot.data();
                 result.id = snapshot.id;
