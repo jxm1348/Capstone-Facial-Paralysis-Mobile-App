dda1d8d7e4f11b3cd37a0b784d32219445e4b334
Mark Peschel <mpeschel10@gmail.com>
2024-03-01 17:08:43 -0500

    Add commands to build a .apk file to deploy_stuff.sh. Specifically, npx expo prebuild -p android, which generates a java/kotlin/something or other project, and sh android/gradlew assembleRelease, which is supposed to compile it into an apk located at android/app/build/outputs/apk/release/app-release.apk Note that you also need to do some other tasks to prepare your computer for a build; in particular, I think you need to install android studio and some specific platform sdk. Add generated android and ios directories to .gitignore. Might have to remove those later if we make changes directly? I don't know how expo works Add "package" field to app.json for some reason. npx expo prebuild did this automatically. I assume the package name doesn't really matter.

--- deploy_stuff.sh
+++ deploy_stuff.sh
@@ -2,10 +2,15 @@
 
 # For compiling, apparently
 export ANDROID_HOME="$HOME/Android/Sdk"
-export PATH="/usr/lib/jvm/java-17-openjdk/bin:$PATH"
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
+[[ "$PATH" != */usr/lib/jvm/java-17-openjdk/bin* ]] && export PATH="/usr/lib/jvm/java-17-openjdk/bin:$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools"
 
-# For publishing to google play store
+# For publishing to google play store, I guess?
 [ ! -f secrets/facial-analytics-test.keystore ] && keytool -genkeypair -v -storetype PKCS12 -keystore secrets/facial-analytics-test.keystore -alias test -keyalg RSA -keysize 2048 -validity 10000
 
+npx expo prebuild -p android
+cd android
+sh ./gradlew assembleRelease
+cd ..
+
+echo "Built android apk. Location should be android/app/build/outputs/apk/release/app-release.apk"
+echo "I was able to install this without the app store by loading it on my phone, then \"opening\" it, then enabling the install unknown apps permission. -M 2024-03-01"
