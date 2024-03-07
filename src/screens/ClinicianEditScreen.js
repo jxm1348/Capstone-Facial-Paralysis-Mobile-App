a7d183bbf0d8d43ff6d88b8e1a789eca16803e68
Mark Peschel <mpeschel10@gmail.com>
2024-03-07 15:58:23 -0500

    Make ClinicianEditScreen work. Mash in some code from ClinicianPatientsScreen to go for a consistent look. Should really break that into a component. "PressablePatient" maybe. Set userId to document.id, since I didn't duplicate that data when I set up the database. Still haven't fixed the subscription stuff there. Also add my android build folder from deploy to the .gitignore.

--- src/screens/ClinicianEditScreen.js
+++ src/screens/ClinicianEditScreen.js
@@ -1,38 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import { collection, } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { Pressable, View, Text, Image, StyleSheet } from 'react-native';
+import { collection, getDocs, } from 'firebase/firestore';
 import { db } from '../state';
+
+function PatientEditPressable({patient, handleEdit}) {
+  return (<Pressable
+    style={styles.patientContainer}
+    onPress={() => handleEdit(patient.uid)}
+    nativeID={`pressable-patient-${patient.uid}`}
+    key={patient.uid}
+  >
+  <View
+    key={patient.uid}
+    style={{
+      flexDirection: 'row',
+      gap: 6,
+      margin: 6,
+    }}
+  >
+    <Image
+      style={styles.patientThumbnail}
+      source={{ uri: patient.thumbnail }}
+    />
+    <View style={{flexGrow: 1, marginHorizontal: 25, flexShrink: 1}}>
+      <View style={{
+        justifyContent: 'center',
+      }}>
+        <Text style={{fontSize: 24}}>{patient.name}</Text>
+        <Text style={{fontSize: 18}}>{patient.email}</Text>
+      </View>
+    </View>
+    <View style={{flexGrow: 1}} />
+  </View>
+  </Pressable>);
+}
 
 const AccountListScreen = ({ navigation }) => {
   const [accounts, setAccounts] = useState([]);
 
-  // useEffect(() => {
-  //   collection(db, 'users')
-  //   const unsubscribe = firestore().collection('users').onSnapshot(querySnapshot => {
-  //     const data = querySnapshot.docs.map(doc => doc.data());
-  //     setAccounts(data);
-  //   });
+  useEffect(() => {
+      async function f() {
+        const userDocuments = await getDocs(collection(db, 'users'));
+        const users = userDocuments
+          .docs
+          .map(document => {
+            const d = document.data()
+            d.uid = document.id;
+            return d;
+          });
+        setAccounts(users);
+      }
+      f();
+  }, []);
 
-  //   return () => unsubscribe();
+    // const unsubscribe = firestore().collection('users').onSnapshot(querySnapshot => {
+    //   const data = querySnapshot.docs.map(doc => doc.data());
+    //   setAccounts(data);
+    // });
+
+    // return () => unsubscribe();
   // }, []);
 
-  const handleEdit = (userId) => {
-    navigation.navigate('EditAccount', { userId });
+  const handleEdit = (uid) => {
+    navigation.navigate('AccountEdit', { uid });
   };
 
-  return (
-    <View>
-      {accounts.map(account => (
-        <View key={account.userId}>
-          <Text>{account.email}</Text>
-          <Button
-            title="Edit"
-            onPress={() => handleEdit(account.userId)}
-          />
-        </View>
-      ))}
-    </View>
-  );
+  return (<View style={{marginHorizontal: 40}}>
+    {accounts.map(patient => (
+      <PatientEditPressable patient={patient} handleEdit={handleEdit} />
+    ))}
+  </View>);
 };
+
+const styles = StyleSheet.create({
+  patientContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 10,
+      padding: 5,
+      borderColor: '#2060dd',
+      borderWidth: 2,
+      borderRadius: 5,
+  }, patientThumbnail: {
+    width: 90,
+    height: 90,
+  },
+});
+
 
 export default AccountListScreen;
