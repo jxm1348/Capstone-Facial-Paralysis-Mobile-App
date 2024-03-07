09ca26f6f5c115c261e8bedba340a0c941a4d9f3
Mark Peschel <mpeschel10@gmail.com>
2024-03-07 16:14:59 -0500

    Fix AccountEditScreen. Fix updateDoc import. I think it was originally functional, but then I changed the param from userId to uid in ClinicianEditAccount. Also implement modular  updateDoc.

--- src/screens/AcccountEditScreen.js
+++ src/screens/AcccountEditScreen.js
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button } from 'react-native';
-// import firestore from '@react-native-firebase/firestore';
-import { doc, } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db, } from '../state';
 
 const EditAccountScreen = ({ route }) => {
-  const { userId } = route.params;
+  const { uid } = route.params;
   const [name, setName] = useState('');
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const userDoc = await getDoc(doc(db, 'users', userId));
+      const userDoc = await getDoc(doc(db, 'users', uid));
       const userData = userDoc.data();
       setName(userData.name);
     };
 
     fetchUserData();
-  }, [userId]);
+  }, [uid]);
 
   const handleSave = async () => {
-    await firestore().collection('users').doc(userId).update({ name });
+    await updateDoc(doc(db, 'users', uid), { name });
     console.log('Account updated successfully!');
   };
 
