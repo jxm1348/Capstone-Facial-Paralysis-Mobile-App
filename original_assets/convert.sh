a429358b3a9b2abd116627fba76b8529e03071a0
Mark Peschel <mpeschel10@gmail.com>
2024-03-15 15:44:44 -0400

    Add images Dr. Lighthall described in her email. Retake eyes-closed, eyebrows-up to get rid of the visible ceiling; split into tightly-closed and gently closed. Delete big-smile because they do big smile with teeth visible. Still only display 7 basic images.

--- original_assets/convert.sh
+++ original_assets/convert.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for n in original_assets/face-mp-*; do
+for IMAGE_NAME in at-rest big-smile-teeth-closed eyebrows-up eyes-closed-gently eyes-closed-tightly lips-puckered lower-teeth-bared nose-wrinkle small-smile; do
+    n=original_assets/face-mp-$IMAGE_NAME.jpg
     echo convert $n -resize 150x200 src/resources/${n/original_assets\/};
     convert $n -resize 150x200 src/resources/${n/original_assets\/};
 done
