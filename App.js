83dd730fc42141967bbe29bd00a52dbb6ba87136
Mark Peschel <mpeschel10@gmail.com>
2024-03-24 16:27:49 -0400

    Test camera now detects face location, kind of. Add useFrameProcessor, which is a JS callback that I think gets compiled with babel into something more native. Add react-native-vision-camera-face-detector library, which hooks up with the MLKit thingy. Add react-native-worklets-core because it seems necessary for vision-camera frameProcessors. Add expo-system-ui because there was an error telling me to. Same with the babel plugin. I sure hope I never have to touch that stuff. I guess the babel is necessary because the frameProcessor has a separate compile step? Certainly, it seems like you can't catch the error from trying to access an undefined object in your frameProcessor function like normal in Javascript.

--- App.js
+++ App.js
@@ -13,20 +13,36 @@
 
 import { useState } from 'react';
 import { Text, View, Pressable } from 'react-native';
-import { Camera, useCameraDevice } from 'react-native-vision-camera'
+import { Camera, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
 
-const tag = '8eab ';
+import { 
+    detectFaces,
+} from 'react-native-vision-camera-face-detector'
+import { Worklets } from 'react-native-worklets-core'
+
+const logTag = '7d3879ca ';
 
 function CameraMaybe({permission}) {
     if (permission !== 'granted') return (<Text>Camera permission must be granted to take selfies</Text>);
-
-    console.log(tag + Camera.getAvailableCameraDevices());
+    
+    const handleFacesDetection = Worklets.createRunInJsFn(result => { 
+        console.log(logTag + 'detection result', result)
+    });
+    
+    const frameProcessor = useFrameProcessor((frame) => {
+        'worklet'
+        console.log(`${logTag}Running face detector!`);
+        detectFaces(frame, handleFacesDetection, {/* detection settings*/});
+    }, [handleFacesDetection]);
+  
+    console.log(logTag + Camera.getAvailableCameraDevices());
     const device = useCameraDevice('front');
     return (
         <Camera
             style={{flex: 1, width: '100%', height: '100%'}}
             device={device}
             isActive={true}
+            frameProcessor={frameProcessor}
         />
     );
 }
@@ -36,13 +52,13 @@ export function App() {
 
     const handleGetCameraPermission = async () => {
         const permission = await Camera.requestCameraPermission()
-        console.log(`Camera permission status: ${permission}`)
+        console.log(`${logTag}Camera permission status: ${permission}`)
     
         if (permission === 'denied') await Linking.openSettings()
         setCameraPermission(permission)
     };
     
-    console.log(tag + `Re-rendering Navigator. Camera: ${cameraPermission}`);
+    console.log(`${logTag}Re-rendering Navigator. Camera: ${cameraPermission}`);
   
     return (
         <View style={{justifyContent:'center', alignItems: 'center', flexGrow: 1}}>
